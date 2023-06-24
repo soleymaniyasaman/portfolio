c0a9cb97037113464658072748a4adcae5c81018
YasaminSoleimani <soleymani.yasaman@yahoo.com>
2023-06-24 23:20:27 +0300

    side bar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import "./App.scss";
 import { Home, About, Contact, ProjectPage, ErrorPage } from "./pages";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/projects" element={<ProjectPage />}></Route>
+        <Route path="/project" element={<ProjectPage />}></Route>
         <Route path="*" element={<ErrorPage />}></Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+import { sideBarMenu, socialIcons } from "../../data/data";
+
+import "./sidebar.scss";
+
+function Sidebar() {
+  return (
+    <aside className="aside">
+      <div className="aside-wrapper">
+        <Link to="/" className="logo-section">
+          <div className="logo">
+            <span>y</span>
+          </div>
+          <span className="switch__color">
+            YASAMAN
+            <br />
+            WEB DEVELOPER
+          </span>
+        </Link>
+        <ul className="side-link">
+          {sideBarMenu.map((link, index) => {
+            const { text, icon, url } = link;
+            return (
+              <li key={index}>
+                <NavLink
+                  to={url}
+                  className={({ isActive }) => {
+                    return isActive ? " nav__links active-links" : "nav__links";
+                  }}
+                >
+                  {icon}
+                  {text}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="social-icon">
+          {socialIcons.map((icons, index) => {
+            const { icon, url } = icons;
+            return (
+              <a href={url} key={index}>
+                {icon}
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
+}
+
+export default Sidebar;

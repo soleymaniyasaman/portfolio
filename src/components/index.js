292a47648a2937e44f9f08e6528a279bf3edf573
YasaminSoleimani <soleymani.yasaman@yahoo.com>
2023-07-01 16:45:06 +0300

    Updates

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Sidebar } from './sidebar/Sidebar'
-export { default as ThemeTemplates } from './ThemingTemplate/ThemeTemplates'
-export { default as Cards } from './skillsCards/Cards'
-export { default as Blast } from './BlastAnimation/Blast'
-export { default as Project } from './projects/Project'
-export { default as Map } from './map/Map'
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './home/Home'
-export { default as About } from './about/About'
-export { default as Contact } from './contact/Contact'
-export { default as ErrorPage } from './errorPage/ErrorPage'
-export { default as ProjectPage } from './project/ProjectPage'

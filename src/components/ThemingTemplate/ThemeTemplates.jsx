4cc68ba748c993d1bdf1502e93140dcd552b8a29
YasaminSoleimani <soleymani.yasaman@yahoo.com>
2023-07-01 23:17:24 +0300

    some changes

--- src/components/ThemingTemplate/ThemeTemplates.jsx
+++ src/components/ThemingTemplate/ThemeTemplates.jsx
@@ -4,7 +4,7 @@ import { BsMoonStars, BsFillSunFill } from "react-icons/bs";
 import { GlobalContext } from "../../hooks/contextAPI";
 
 import { BsXLg } from "react-icons/bs";
-import "./themeTemplate.scss";
+import "./themetemplate.scss";
 
 const ThemeTemplates = React.memo(() => {
   const {
--- src/pages/project/ProjectPage.jsx
+++ src/pages/project/ProjectPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Fade from "react-reveal/Fade";
 import { projects } from "./index";
 import { Project, Blast } from "../../components";
-import "./projectPage.scss";
+import "./projectpage.scss";
 
 const ProjectPage = () => {
   const [letterClass, setLetterClass] = React.useState("text-animate");
